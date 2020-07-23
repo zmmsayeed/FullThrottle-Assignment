b2c80ba6e755e10864e056a1a392eec1c11483a0
Zimam Sayeed <zmmsayeed@gmail.com>
2020-07-24 03:35:17 +0530

    If no activity is recorded for the selected date, it shows a message on the modal

--- client/src/Components/Modal/index.js
+++ client/src/Components/Modal/index.js
@@ -23,103 +23,120 @@ class Modal extends Component {
         this.setState({ filtered: true, filterDate: dateString })
     }
 
+    sameDayFilter = (date) => {
+        let filtered = this.props.user.activity_periods.filter(activity => {
+            return moment(activity.start_time, "MMM DD YYYY hh:mm A").format("MMMM DD, YYYY") === date
+        })
+
+        return filtered;
+    }
+
+    renderFilterData = () => {
+        let filteredActivity = this.props.user.activity_periods.filter(ent => {
+            return moment(ent.start_time, "MMM DD YYYY hh:mmA").format("YYYY-MM-DD") === moment(this.state.filterDate, 'YYYY-MM-DD').format("YYYY-MM-DD")
+        })
+        console.log(filteredActivity)
+
+        if (filteredActivity.length > 0) {
+            return filteredActivity.map(activity => {
+                let date = moment(activity.start_time, "MMM DD YYYY hh:mmA").format("MMMM DD, YYYY");
+
+                if (activityArr.includes(date)) {
+                    return null
+                }
+                else {
+                    activityArr.push(date);
+
+                    let filtered = this.sameDayFilter(date)
+
+                    return <div className="mb-3">
+                        <p className="modalDate mb-1">
+                            Date: {date}
+                        </p>
+                        <table>
+                            {
+                                filtered.map((entry, index) => {
+                                    activityArr.push(entry);
+                                    if (index === 0) {
+                                        return (<>
+                                            <tr>
+                                                <th>Start Time</th>
+                                                <th>End Time</th>
+                                            </tr>
+                                            <tr>
+                                                <td>{moment(entry.start_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
+                                                <td>{moment(entry.end_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
+                                            </tr>
+                                        </>)
+                                    }
+                                    else {
+                                        return (<>
+                                            <tr>
+                                                <td>{moment(entry.start_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
+                                                <td>{moment(entry.end_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
+                                            </tr>
+                                        </>)
+                                    }
+                                })
+                            }
+                        </table>
+                    </div>
+                }
+            })
+        }
+        else {
+            return <p className="noActivityPara">No activity recorded for selected date</p>
+        }
+    }
+
     render() {
+        return (
+            <Drawer
+                width={window.innerWidth > 900 ? '50%' : '95%'}
+                // title={this.props.user.real_name}
+                placement="right"
+                // closable={true}
+                onClose={() => {
+                    activityArr = [];
+                    this.setState({ filtered: false })
+                    this.props.onClose(this.props.user)
+                }}
+                visible={this.props.visible}
+            >
 
-        if (this.props.visible) {
-            return (
-                <Drawer
-                    width={window.innerWidth > 900 ? '50%' : '95%'}
-                    // title={this.props.user.real_name}
-                    placement="right"
-                    // closable={true}
-                    onClose={() => {
-                        activityArr = [];
-                        this.setState({ filtered: false })
-                        this.props.onClose(this.props.user)
-                    }}
-                    visible={this.props.visible}
-                >
+                {
+                    !this.props.user
+                        ? ""
+                        : <>
+                            <p className="modalHeader">
+                                {this.props.user.real_name}
+                            </p>
+                            <p className="modalId">
+                                ID: {this.props.user.id}
+                            </p>
+                            <hr />
 
-                    {
-                        !this.props.user
-                            ? ""
-                            : <>
-                                <p className="modalHeader">
-                                    {this.props.user.real_name}
-                                </p>
-                                <p className="modalId">
-                                    ID: {this.props.user.id}
-                                </p>
-                                <hr />
-
-                                <div className="row">
-                                    <div className="col-6 mt-2">
-                                        <p className="activityHeading">Activity Logs:</p>
-                                    </div>
-                                    <div className="col-6 text-right">
-                                        <DatePicker size="large" 
-                                            className="w-100" 
-                                            placeholder="Activity Date" 
-                                            onChange={this.dateChange}
-                                        />  
-                                    </div>
+                            <div className="row">
+                                <div className="col-6 mt-2">
+                                    <p className="activityHeading">Activity Logs:</p>
                                 </div>
+                                <div className="col-6 text-right">
+                                    <DatePicker size="large"
+                                        className="w-100"
+                                        placeholder="Activity Date"
+                                        onChange={this.dateChange}
+                                    />
+                                </div>
+                            </div>
 
-                                {
-                                    this.state.filtered
-                                    ? !this.props.user.activity_periods ? ""
-                                    : this.props.user.activity_periods.filter(ent => {
-                                        return moment(ent.start_time, "MMM DD YYYY hh:mmA").format("YYYY-MM-DD") === moment(this.state.filterDate, 'YYYY-MM-DD').format("YYYY-MM-DD")
-                                    }).map(activity => {
-                                        let date = moment(activity.start_time, "MMM DD YYYY hh:mmA").format("MMMM DD, YYYY")
+                            {
+                                this.state.filtered
+                                    ? !this.props.user.activity_periods.length > 0
+                                        ? <p className="noActivityPara">No Activities Recorded for this User</p>
+                                        : this.renderFilterData()
 
-                                        if (activityArr.includes(date)) {
-                                            return null
-                                        }
-                                        else {
-                                            activityArr.push(date);
-                                            return (
-                                                <div className="mb-3">
-                                                    <p className="modalDate mb-1">
-                                                        Date: {date}
-                                                    </p>
-                                                    <table>
-
-                                                        {
-                                                            this.props.user.activity_periods.filter(activity => {
-                                                                return moment(activity.start_time, "MMM DD YYYY hh:mm A").format("MMMM DD, YYYY") === date
-                                                            }).map((entry, index) => {
-                                                                activityArr.push(entry);
-                                                                if (index === 0) {
-                                                                    return (<>
-                                                                        <tr>
-                                                                            <th>Start Time</th>
-                                                                            <th>End Time</th>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <td>{moment(entry.start_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
-                                                                            <td>{moment(entry.end_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
-                                                                        </tr>
-                                                                    </>)
-                                                                }
-                                                                else {
-                                                                    return (<>
-                                                                        <tr>
-                                                                            <td>{moment(entry.start_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
-                                                                            <td>{moment(entry.end_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
-                                                                        </tr>
-                                                                    </>)
-                                                                }
-                                                            })
-                                                        }
-
-                                                    </table>
-                                                </div>
-                                            )
-                                        }
-                                    })
-                                    
-                                    : !this.props.user.activity_periods ? ""
+                                    : !this.props.user.activity_periods.length > 0
+                                        ? <p className="noActivityPara">No Activities Recorded for this User</p>
                                         : this.props.user.activity_periods.map(activity => {
                                             let date = moment(activity.start_time, "MMM DD YYYY hh:mm A").format("MMMM DD, YYYY")
 
@@ -168,19 +185,10 @@ class Modal extends Component {
                                                 )
                                             }
                                         })
-
-                                }</>
-
-                    }
-
-                </Drawer>
-            )
-        }
-        else {
-            return (
-                <div></div>
-            )
-        }
+                            }</>
+                }
+            </Drawer>
+        )
     }
 }
 
