ec88a16c26eea4a137777a700e02778af8bf9f71
Zimam Sayeed <zmmsayeed@gmail.com>
2020-07-23 16:59:50 +0530

    Added filter functionality using date in activity logs of a user

--- client/src/Components/Modal/index.js
+++ client/src/Components/Modal/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 // import Drawer from Ant Design
-import { Drawer } from 'antd';
+import { Drawer, DatePicker } from 'antd';
 
 // importing stylesheet
 import './style.css';
@@ -10,6 +10,19 @@ import './style.css';
 let activityArr = [];
 
 class Modal extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filtered: false
+        }
+    }
+
+    dateChange = (date, dateString) => {
+        activityArr = [];
+        this.setState({ filtered: true, filterDate: dateString })
+    }
+
     render() {
 
         if (this.props.visible) {
@@ -21,6 +34,7 @@ class Modal extends Component {
                     // closable={true}
                     onClose={() => {
                         activityArr = [];
+                        this.setState({ filtered: false })
                         this.props.onClose(this.props.user)
                     }}
                     visible={this.props.visible}
@@ -38,11 +52,74 @@ class Modal extends Component {
                                 </p>
                                 <hr />
 
-                                <p className="activityHeading">Activity Logs:</p>
+                                <div className="row">
+                                    <div className="col-6 mt-2">
+                                        <p className="activityHeading">Activity Logs:</p>
+                                    </div>
+                                    <div className="col-6 text-right">
+                                        <DatePicker size="large" 
+                                            className="w-100" 
+                                            placeholder="Activity Date" 
+                                            onChange={this.dateChange}
+                                        />  
+                                    </div>
+                                </div>
 
                                 {
+                                    this.state.filtered
+                                    ? !this.props.user.activity_periods ? ""
+                                    : this.props.user.activity_periods.filter(ent => {
+                                        return moment(ent.start_time, "MMM DD YYYY hh:mmA").format("YYYY-MM-DD") === moment(this.state.filterDate, 'YYYY-MM-DD').format("YYYY-MM-DD")
+                                    }).map(activity => {
+                                        let date = moment(activity.start_time, "MMM DD YYYY hh:mmA").format("MMMM DD, YYYY")
 
-                                    !this.props.user.activity_periods ? ""
+                                        if (activityArr.includes(date)) {
+                                            return null
+                                        }
+                                        else {
+                                            activityArr.push(date);
+                                            return (
+                                                <div className="mb-3">
+                                                    <p className="modalDate mb-1">
+                                                        Date: {date}
+                                                    </p>
+                                                    <table>
+
+                                                        {
+                                                            this.props.user.activity_periods.filter(activity => {
+                                                                return moment(activity.start_time, "MMM DD YYYY hh:mm A").format("MMMM DD, YYYY") === date
+                                                            }).map((entry, index) => {
+                                                                activityArr.push(entry);
+                                                                if (index === 0) {
+                                                                    return (<>
+                                                                        <tr>
+                                                                            <th>Start Time</th>
+                                                                            <th>End Time</th>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td>{moment(entry.start_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
+                                                                            <td>{moment(entry.end_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
+                                                                        </tr>
+                                                                    </>)
+                                                                }
+                                                                else {
+                                                                    return (<>
+                                                                        <tr>
+                                                                            <td>{moment(entry.start_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
+                                                                            <td>{moment(entry.end_time, "MMM DD YYYY hh:mm A").format("hh:mmA")}</td>
+                                                                        </tr>
+                                                                    </>)
+                                                                }
+                                                            })
+                                                        }
+
+                                                    </table>
+                                                </div>
+                                            )
+                                        }
+                                    })
+                                    
+                                    : !this.props.user.activity_periods ? ""
                                         : this.props.user.activity_periods.map(activity => {
                                             let date = moment(activity.start_time, "MMM DD YYYY hh:mm A").format("MMMM DD, YYYY")
 
@@ -108,10 +185,3 @@ class Modal extends Component {
 }
 
 export default Modal;
-
-
-                                        // this.props.user.activity_periods.filter(activity => {
-                                        //     return moment(activity.start_time, "MMM DD YYYY hh:mm A").format("MMMM DD, YYYY") === date
-                                        // }).map(entry => {
-                                        //     return <p>Start Time: {entry.start_time} - End Time: {entry.end_time}</p>
-                                        // })
